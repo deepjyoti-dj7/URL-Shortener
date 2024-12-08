99fc9eac4be11cbc1f4ca76b0baeb15aabd02296
Deepjyoti <deepjyoti0072@gmail.com>
2024-12-08 17:51:27 +0530

    Added basic admin authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,23 @@
 const { getUser } = require("../service/auth");
 
-async function restrictToLoggedinUserOnly(req, res, next) {
-  const userUid = req.headers["authorization"];
+function checkForAuthentication(req, res, next) {
+  const tokenCookie = req.cookies?.token;
+  req.user = null;
+  if (!tokenCookie) return next();
 
-  if (!userUid) return res.redirect("/login");
-  const token = userUid.split("Bearer ")[1];
+  const token = tokenCookie;
   const user = getUser(token);
 
-  if (!user) return res.redirect("/login");
-
   req.user = user;
-  next();
+  return next();
 }
 
-async function checkAuth(req, res, next) {
-  const userUid = req.headers["authorization"];
-  const token = userUid.split("Bearer ")[1];
-
-  const user = getUser(token);
-  req.user = user;
-  next();
+function restrictTo(roles = []) {
+  return function (req, res, next) {
+    if (!req.user) return res.redirect("/login");
+    if (!roles.includes(req.user.role)) return res.end("Not Authorized");
+    next();
+  };
 }
 
-module.exports = { restrictToLoggedinUserOnly, checkAuth };
+module.exports = { checkForAuthentication, restrictTo };
--- routes/staticRouter.js
+++ routes/staticRouter.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const URL = require("../models/url");
+const { restrictTo } = require("../middlewares/auth");
 
-router.get("/", async (req, res) => {
-  console.log(req.user);
-  if (!req.user) return res.redirect("/login");
+router.get("/admin/urls", restrictTo(["ADMIN"]), async (req, res) => {
+  const urls = await URL.find({});
+  return res.render("home", {
+    urls,
+  });
+});
+
+router.get("/", restrictTo(["NORMAL", "ADMIN"]), async (req, res) => {
   const allurls = await URL.find({ createdBy: req.user._id });
   return res.render("home", {
     urls: allurls,
